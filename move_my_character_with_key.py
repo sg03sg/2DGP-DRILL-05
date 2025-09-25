773b73e2b64241d43a9e251155fe0f85be70e464
jeoungseunggyun(E320-32) <csg1221sg@gmail.com>
2025-09-25 17:09:54 +0900

    이벤트 함수 및 그리기 함수 1차 선언

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -1,22 +1,56 @@
 from pico2d import *
 
 open_canvas()
+TUK_WIDTH, TUK_HEIGHT = 1280,1024
 tuk_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 
 running = True
-x==800//2
-dir =0
+x = TUK_WIDTH//2
+y = TUK_HEIGHT//2
+dir_x =0
+dir_y =0
 animations =0
 
-def draw_character():
-    pass
+def draw_character(animation):
+    clear_canvas()
+    tuk_ground.draw(400,30)
+    character.clip_draw(animation*100,100,100,100,x,90)
+    update_canvas()
+    delay(0.05)
+
 
 def handle_events():
-   pass
+    global running
+    global dir_x,dir_y
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir_x = 2
+            elif event.key == SDLK_LEFT:
+                dir_x = -2
+            elif event.key == SDLK_UP:
+                dir_y = 1
+            elif event.key == SDLK_DOWN:
+                dir_y = -1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir_x -= 1
+            elif event.key == SDLK_LEFT:
+                dir_x += 1
+            elif event.key == SDLK_UP:
+                dir_y -= 1
+            elif event.key == SDLK_DOWN:
+                dir_y += 1
 
 while running:
-    draw_character()
+    draw_character(animations)
 
     handle_events()
 
