ff0273d1f644d1000570a8946489fb32cabe23b6
jeoungseunggyun(E320-32) <csg1221sg@gmail.com>
2025-09-25 16:44:23 +0900

    마우스를 이용한 캐릭터 이동구현

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -1,35 +1,37 @@
 from pico2d import *
 
 
-# fill here
-
-
+TUK_WIDTH, TUK_HEIGHT = 1280,1024
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+tuk_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 
 
 def handle_events():
-    global running
-
-    # fill here
+    global running,x,y
 
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        # fill here
-
+        elif event.type == SDL_MOUSEMOTION:
+            x,y = event.x, TUK_HEIGHT - event.y
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
 
 running = True
 frame = 0
 
-# fill here
+x,y = TUK_WIDTH //2, TUK_HEIGHT //2
+hide_cursor()
 
 
 
 while running:
     clear_canvas()
 
-    # fill here
+    tuk_ground.draw(TUK_WIDTH//2,TUK_HEIGHT//2)
+    character.clip_draw(frame*100,100*1,100,100,x,y)
 
     update_canvas()
     handle_events()
