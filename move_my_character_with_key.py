0cad0b26d2eb9ee7d13d0198eb08471eeed373a1
seunggyunJung <csg1221sg@gmail.com>
2025-09-25 22:56:50 +0900

    캐릭터 애니메이션 구현 및 키보드 이벤트 별 캐릭터 애니메이션 변경 로직 추가

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -1,21 +1,24 @@
 from pico2d import *
 
-open_canvas()
 TUK_WIDTH, TUK_HEIGHT = 1280,1024
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
+
 
 running = True
 x = TUK_WIDTH//2
 y = TUK_HEIGHT//2
-dir_x =0
+dir_x =1
 dir_y =0
 animations =0
+frame =0
 
-def draw_character(animation):
+def draw_character(animation,frame):
+    global x,y
     clear_canvas()
-    tuk_ground.draw(400,30)
-    character.clip_draw(animation*100,100,100,100,x,90)
+    tuk_ground.draw(TUK_WIDTH//2,TUK_HEIGHT//2)
+    character.clip_draw(frame*100,animation,100,100,x,y)
     update_canvas()
     delay(0.05)
 
@@ -34,9 +37,9 @@ def handle_events():
             elif event.key == SDLK_LEFT:
                 dir_x = -2
             elif event.key == SDLK_UP:
-                dir_y = 1
+                dir_y += 1
             elif event.key == SDLK_DOWN:
-                dir_y = -1
+                dir_y -= -1
             elif event.key == SDLK_ESCAPE:
                 running = False
         elif event.type == SDL_KEYUP:
@@ -50,7 +53,18 @@ def handle_events():
                 dir_y += 1
 
 while running:
-    draw_character(animations)
+    if dir_x ==2:
+        animations = 100
+    elif dir_x == -2:
+        animations = 0
+    elif dir_x == 1:
+        animations = 300
+    elif dir_x == -1:
+        animations = 200
+
+    frame = (frame + 1) % 8
+
+    draw_character(animations,frame)
 
     handle_events()
 
