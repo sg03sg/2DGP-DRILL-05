b522534ff4331e30d05344691315472e190331eb
jeoungseunggyun(E320-32) <csg1221sg@gmail.com>
2025-09-25 16:53:27 +0900

    반복문 및 함수 설정

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -1,1 +1,23 @@
-from
+from pico2d import *
+
+open_canvas()
+tuk_ground = load_image('TUK_GROUND.png')
+character = load_image('animation_sheet.png')
+
+running = True
+x==800//2
+dir =0
+animations =0
+
+def draw_character():
+    pass
+
+def handle_events():
+   pass
+
+while running:
+    draw_character()
+
+    handle_events()
+
+close_canvas()
